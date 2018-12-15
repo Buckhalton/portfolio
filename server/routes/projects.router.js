0835c070acc4b93ad2b5ed42cd3a8d28e19706d3
admin <admin@admins-MacBook-Pro.local>
2018-12-15 10:43:27 -0600

    completes serverside routing.

--- server/routes/projects.router.js
+++ server/routes/projects.router.js
@@ -15,11 +15,30 @@ router.get('/api/projects', (req, res) => {
 })
 
 router.post('/api/projects', (req, res) => {
-
+    const project = req.body;
+    const queryText = 'INSERT INTO "projects" ("name", "description", "website", "github", "date_completed", "tag_id") ' +
+    'VALUES ($1, $2, $3, $4, $5, $6);';
+    pool.query(queryText, [project.name, project.description, project.website, project.github, project.date_completed, project.tag_id])
+    .then(result => {
+        res.sendStatus(200);
+    })
+    .catch(error => {
+        console.log('error in post', error);
+        res.sendStatus(500);
+    })
 })
 
 router.delete('/api/projects/:id', (req, res) => {
-
+    const id = req.params.id;
+    const queryText = 'DELETE FROM "projects" WHERE id =$1;';
+    pool.query(queryText, [id])
+    .then(result => {
+        res.sendStatus(202);
+    })
+    .catch(error => {
+        console.log('error in delete', error);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
