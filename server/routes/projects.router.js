d39a09e495f8e002b6ac42ad2c72846be9fbde2d
admin <admin@admins-MacBook-Pro.local>
2018-12-15 00:09:28 -0600

    adds server structure.

---
+++ server/routes/projects.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/api/projects', (req, res) => {
+
+})
+
+router.post('/api/projects', (req, res) => {
+
+})
+
+router.delete('/api/projects/:id', (req, res) => {
+
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+/** ROUTE INCLUDES **/
+const projectsRouter = require('./routes/projects.router');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
+app.use('/api/projects', projectsRouter)
 
+// APP SET //
+const PORT = process.env.PORT || 5000;
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
