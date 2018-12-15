b9e89cf947d332de6d32de64b84d64b3ae9f758e
admin <admin@admins-MacBook-Pro.local>
2018-12-15 10:29:07 -0600

    adds server get route.

--- server/routes/projects.router.js
+++ server/routes/projects.router.js
@@ -3,7 +3,15 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/api/projects', (req, res) => {
-
+    const queryText = 'SELECT * FROM "projects" LEFT JOIN "tags" ON "projects".tag_id = "tags".id;';
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error on query:', error);
+        res.sendStatus(500);
+    })
 })
 
 router.post('/api/projects', (req, res) => {
