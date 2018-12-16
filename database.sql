e47d8051b1f6c8a5158c2cc362efbc040aff5001
buckhalton <isaiah.buckhalton@gmail.com>
2018-12-16 16:23:57 -0600

    adds insert query to db file.

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 -- Database name: portfolio
 
 CREATE TABLE "tags" (
-    "id" SERIAL PRIMARY KEY,
-    "name" varchar(255) NOT NULL
+    "tag_id" SERIAL PRIMARY KEY,
+    "tagname" varchar(255) NOT NULL
 );
 
 CREATE TABLE "projects" (
@@ -16,5 +16,8 @@ CREATE TABLE "projects" (
     "tag_id" INT REFERENCES "tags"
 );
 
-INSERT INTO "tags" ("name") 
+INSERT INTO "tags" ("tagname") 
 VALUES ('React'), ('jQuery'), ('Node'), ('SQL'), ('Redux'), ('HTML');
+
+INSERT INTO "projects" ("name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
+VALUES ('Feedback App', 'A basic feedback app that uses react, redux, and postgreSQL for the database.', '/images/feedback.png', 'https://shielded-badlands-39219.herokuapp.com/#/', 'https://github.com/Buckhalton/Weekend-Challenge-5', '2018-12-08', 1);
